1e1914055b937c4881e4347fe38e3b7b49068b11
Thagana <samuelmthwa79@gmail>
2022-04-26 23:55:05 +0200

    local storage

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,21 +1,35 @@
 import * as React from "react";
 import Description from "../Description";
 import IArticle from "../../interface/Article.interface";
+import Notification from 'antd/es/notification';
+
+import { Store } from '../../helpers/LocalArticleStore';
 
 import "./Card.scss";
 
 interface Props {
   item: IArticle;
+  isLocal: boolean;
 }
 
 export default function Article(props: Props) {
-  const { item } = props;
+  const { item, isLocal } = props;
   const handleClick = () => {
     window.open(item.url, "_blank");
   };
 
-  const handleBookMark = () => {
-    console.log('book marked!')
+  const handleBookMark = async () => {
+    try {
+      await Store.addArticles(item);
+      Notification.success({
+        message: 'Successfully added a bookmark item'
+      })
+    } catch (error) {
+      console.log(error);
+      Notification.error({
+        message: 'Something went wrong please try again',
+      })
+    }
   };
 
   return (
@@ -30,6 +44,7 @@ export default function Article(props: Props) {
             publishedAt={item.publishedAt}
             handleBookMark={handleBookMark}
             handleClick={handleClick}
+            isLocal={isLocal}
           />
         </p>
       </div>
--- src/views/Save/Save.tsx
+++ src/views/Save/Save.tsx
@@ -1,14 +1,33 @@
 import * as  React from 'react'
 
 import Template from '../Template';
+import { Store } from '../../helpers/LocalArticleStore';
 
 import './Save.scss';
 
+import Article from '../../interface/Article.interface';
+import ArticleList from '../../components/CardList';
+
 export default function Save() {
+  const [articles, setArticles] = React.useState<Article[]>([]);
+  
+  const fetchLocalStore = async () => {
+    try {
+      const store = await Store.getArticles();
+      setArticles(store);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  React.useEffect(() => {
+    fetchLocalStore();
+  },[]);
+
   return (
     <Template activeKey="2">
       <div className='save-container'>
-        Coming Soon
+        <ArticleList data={articles} isLocal={true} />
       </div>
     </Template>
   )
